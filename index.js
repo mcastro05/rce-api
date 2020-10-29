88f128d2abea9acf422629b5e7b81a06ab98764f
Melissa Castro <melissa@soluntech.com>
2020-10-29 17:21:33 -0500

    first commit

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+const router = express.Router();
+
+router.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
